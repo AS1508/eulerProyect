fed30024dfa7a2692a1db0d73325748f787b70cf
Sebastián Martínez <70720135+AS1508@users.noreply.github.com>
2023-12-28 14:43:23 -0300

    Resolutions 28/12

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,10 @@
+﻿const int maximo = 1000;
+int total = 0;
+
+for(int i = 0; i<maximo; i++){
+    if(i%3 == 0 || i%5 == 0){
+        total += i;
+    }
+}
+
+Console.WriteLine($">> {total}");
---
+++ Exercise 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Exercise::By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+const double maximo = 4000000;
+double fibonacci = 0, fibonacciSuma = 0;
+double n1 = 0, n2 = 1;
+
+while(fibonacciSuma < maximo){
+    fibonacci = n1 + n2;
+    if(fibonacci%2 == 0){
+        fibonacciSuma += fibonacci;
+        n1 = n2;
+        n2 = fibonacci;
+    }else{
+        n1 = n2;
+        n2 = fibonacci;
+    }
+}
+
+Console.WriteLine($">> {fibonacciSuma}");
