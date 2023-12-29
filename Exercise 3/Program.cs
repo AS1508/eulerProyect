52e2123f7658ff3e8c26acaf60205376f8450b06
Sebastián Martínez <70720135+AS1508@users.noreply.github.com>
2023-12-29 10:43:20 -0300

    upd

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Exercise::Found the largest prime factor of the number {termninar}
+const long numero = 600851475143;
+long factor,maximo = 0;
+int b = 0;
+
+for(long i = 1; i<numero; i++){
+    if(numero%i == 0){
+        factor = i;
+        if(b == 0 || factor > maximo){
+            b = 1;
+            maximo = factor;
+        }
+    }
+}
+
+Console.WriteLine($">> {maximo}");
